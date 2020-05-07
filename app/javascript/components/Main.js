f8b30225b3a56b1787eea4d9add43bca5e648ce5
Daniel Moros <aceim.development@gmail.com>
2020-05-07 03:19:55 -0400

    Agregado React y modelo Item

---
+++ app/javascript/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header"
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Bienvenido a Aceim React App </h1>
+        <Header />
+      </div>
+    );
+  }
+}
+export default Main
