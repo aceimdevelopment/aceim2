e680adc16fe97b61f0d1ff4deb870330bcbc402a
Daniel Moros <aceim.development@gmail.com>
2020-05-14 18:09:06 -0400

    Config Images

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,6 +1,15 @@
-import React, {useRef} from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import LogoAceim from 'images/logo_aceim.png';
+import LogoEIM from 'images/logo_eim.png';
+import LogoEIMBlue from 'images/logo_eim_blue.png';
+import LogoEIMWhite from 'images/logo_eim_white.png';
+
 class Header extends React.Component {
   render () {
 	// const largo = useRef(null);
@@ -8,6 +17,14 @@ class Header extends React.Component {
 
     return (
       <React.Fragment>
+		<Button>
+			<span><i className="fa fa-camera-retro"></i></span>
+			<FontAwesomeIcon icon={faCoffee} />
+			Hello Gente!
+			<img src={LogoEIMBlue} width='100px' />
+			<img src={LogoEIMWhite} width='100px' />
+		</Button>
+		<Image src={LogoEIM} width='100px' rounded />
       	<h1>Encabezado</h1>
 {/*		<input type='text' ref={largo} />
 		<input type='text' ref={ancho} />*/}
