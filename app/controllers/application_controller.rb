15c653efe9f8d3bc8f029cb4cf83df885f387b9c
Daniel Moros <aceim.development@gmail.com>
2020-05-20 15:31:54 -0400

    Setup Rails Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   	before_action :authenticate_user!
 
 	def after_sign_in_path_for(resource)
-		dashboard_index_path
+		# dashboard_index_path
+		rails_admin_path
 	end
 
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,9 +8,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
+    user ||= User.new # guest user (not logged in)
+    can :manege, :all
+    # if user.superadmin_role?
+    #   can :manage, :all
+    #   can :access, :rails_admin       # only allow admin users to access Rails Admin
+    #   can :manage, :dashboard         # allow access to dashboard
+    # end
+    # if user.supervisor_role?
+    #   can :manage, User
+    # end
     #   if user.superadmin_role?
     #     can :manage, :all
     #   else
