5a460cb974f5229cced867c45870e1af3bbc577d
Daniel Moros <aceim.development@gmail.com>
2020-07-07 19:25:19 -0400

    Ajuste

--- app/javascript/components/Home/MainText.js
+++ app/javascript/components/Home/MainText.js
@@ -4,7 +4,7 @@ class MainText extends React.Component {
     return (
       <div className='text-center align-middle mt-4' >
       <h5 className=' mb-3'> En FUNDEIM seguimos comprometidos con nuestra pasión: promover y motivar el aprendizaje de idiomas extranjeros en nuestro país. </h5>
-      <h5 className=' mb-3'> Estamos afinando los detalles del nuevo programa de cursos FUNDEIM ONLINE que consta de 18 clases por idioma y nivel. </h5>
+      <h5 className=' mb-3'> Estamos afinando los detalles del nuevo programa de cursos FUNDEIM ONLINE que consta de 12 clases por idioma y nivel. </h5>
       <h5 className=' mb-3'> Toda la información estará disponible aquí y en nuestra cuenta de twitter <a href="https://twitter.com/Fundeim">@fundeim</a>. </h5>
       </div>
     );
