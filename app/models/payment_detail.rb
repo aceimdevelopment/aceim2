b3ab18a4f67851fabcf64aeea47b8718fe0b443c
Daniel Moros <aceim.development@gmail.com>
2020-07-13 16:50:52 -0400

    Implementado MySearch

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,4 +1,6 @@
 class PaymentDetail < ApplicationRecord
+	
+  # ========== ASSOCIATION ============ #
 	belongs_to :academic_record, inverse_of: :payment_detail
 	has_one :student, through: :academic_record
 	has_one :user, through: :student
@@ -9,6 +11,7 @@ class PaymentDetail < ApplicationRecord
 
 	has_one_attached :backup_file
 
+  # ========== VALIDATIONS ============ #
 	validates :bank_account, presence: true
 	validates :transaction_type, presence: true
 	validates :transaction_number, presence: true
@@ -21,10 +24,20 @@ class PaymentDetail < ApplicationRecord
 
 	# belongs_to :pci_escuela, foreign_key: 'pci_escuela_id', class_name: 'Escuela', optional: true
 
+  # ========== ENUM TYPE ============ #
 	enum transaction_type: [:transferencia, :divisas]
+
 
 	# before_destroy :check_for_files#, prepend: true
 
+
+  # ========== SCOPE ============ #
+
+	# scope :search, lambda { |clave| 
+	# 	where("MATCH(ci, nombres, apellidos, email, telefono_habitacion, telefono_movil) AGAINST('#{clave}')")
+	# }
+
+	scope :my_custom_search, -> (keyword) {joins(academic_record: {student: :user}).where("students.personal_identity_document LIKE ? or users.name LIKE ? or users.email LIKE ? or users.last_name LIKE ?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")}
 	scope :unread_report, -> {where(read_report: false)}
 	scope :unread_confirmation, -> {where(read_confirmation: false)}
 
@@ -33,6 +46,7 @@ class PaymentDetail < ApplicationRecord
 
 	scope :todos, -> {where('0 = 0')}
 
+  # ========== FUNCTIONS ============ #
 	def name
 		aux = transaction_number
 		aux += academic_record.desc_to_pay if academic_record
@@ -125,6 +139,7 @@ class PaymentDetail < ApplicationRecord
 
 		list do
 			scopes [:todos, :preinscritos, :confirmados]
+			search_by :my_custom_search
 			field :transaction_number do
 				label '# Transacción'
 			end
@@ -141,7 +156,7 @@ class PaymentDetail < ApplicationRecord
 				label 'Inscripción'
 				html_attributes do
 					{:style => "width: 50%"}
-				end				
+				end
 			end
 
 			field :mount do
@@ -150,6 +165,7 @@ class PaymentDetail < ApplicationRecord
 
 			field :created_at do
 				label 'Fecha'
+				strftime_format "%d/%m/%Y"
 			end
 
 			field :confirm do
