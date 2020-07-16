db25aaa5b043fdfa502da831a6bc46e5dde2c4d6
Daniel Moros <aceim.development@gmail.com>
2020-07-15 23:31:05 -0400

    Ajustes

--- app/models/my_canvas.rb
+++ app/models/my_canvas.rb
@@ -4,4 +4,13 @@ class MyCanvas
   def self.connect
     Canvas::API.new(host: ENV['CANVAS_HOST'], token: ENV['CANVAS_TOKEN'])
   end
+
+
+  def create_section_on_canvas(section)
+  	# course_id = section.url_classroom_canvas
+  	course_id ||= section.course_period.id_canvas
+  	self.post("/api/v1/courses/#{course_id}/sections", {course_section: section.name_canvas})
+
+  end
+
 end
