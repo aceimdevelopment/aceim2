70e736bb8889bf3d63faf19fe05c09107fae0573
Daniel Moros <aceim.development@gmail.com>
2020-08-24 08:54:20 -0400

    Ajuste

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,28 @@ import "../stylesheets/application"
 document.addEventListener("turbolinks:load", () => {
 	$('[data-toggle="tooltip"]').tooltip()
 	$('[data-toggle="popover"]').popover()
+	$('.confirmBtn').on('click', function() {
+		let ele = $(this);
+		let url = ele.attr('url');
+		let descUser = ele.attr('desc_user');
+		let descInscrip = ele.attr('desc_inscrip');
+
+		let textConfirm = `<b>¿Confirma que deseas completar la preinscripcion de ${descUser} en el curso: ${descInscrip} y de que acepta la siguiente normativa ?</b></br></br>`
+
+		let normative = "<p>1.  Es obligatorio leer con detenimiento toda la información suministrada en el mensaje de inicio y en el módulo introductorio de su aula en CANVAS. Si tienen alguna duda sobre ACEIM o CANVAS,  o presentan algún inconveniente con el programa en general, deben contactar de inmediato a su instructor o a FUNDEIM por el correo fundeimucv@gmail.com</p>"
+		normative += "<p>2.  La participación de los estudiantes en las actividades del foro es obligatoria y será tomada como asistencia a clase. La ausencia en el foro por 2 semanas, no necesariamente de manera consecutiva, tendrá como consecuencia la pérdida del curso por inasistencia.</p>"
+		normative += "<p>3.  La calificación mínima aprobatoria es de 15 puntos. La evaluación será continua y dinámica. Encontrará mayor información en el cronograma del curso incluido en el módulo introductorio del nivel.</p>"
+		normative += "<p>4.  El programa ONLINE consta de solo actividades asíncronas, es decir, no serán clases en vivo,  para que cada estudiante pueda organizarse y buscar el tiempo y la conexión para seguir formándose a su ritmo y en el horario de su preferencia; sin embargo, cada estudiante debe completar dos clases por semana, además de realizar las tareas asignadas y los exámenes semanales programados.</p>"
+
+		$('#confirmDialog #content').html(textConfirm + normative);
+
+		$('#confirmDialog #confirmLink').attr('href', url);
+		$('#confirmDialog').modal();
+
+	})
 })
+
+
 
 
 $(document).ready(function() {
@@ -39,25 +60,6 @@ $(document).ready(function() {
 		node.val(node.val().replace(/[^0-9]/g, ''));
 	});
 
-	$('.confirmBtn').on('click', function() {
-		let ele = $(this);
-		let url = ele.attr('url'); 
-		let descUser = ele.attr('desc_user');
-		let descInscrip = ele.attr('desc_inscrip');
-
-		let textConfirm = `<b>¿Confirma que deseas completar la preinscripcion de ${descUser} en el curso: ${descInscrip} y de que acepta la siguiente normativa ?</b></br></br>`
-
-		let normative = "<p>1.  Es obligatorio leer con detenimiento toda la información suministrada en el mensaje de inicio y en el módulo introductorio de su aula en CANVAS. Si tienen alguna duda sobre ACEIM o CANVAS,  o presentan algún inconveniente con el programa en general, deben contactar de inmediato a su instructor o a FUNDEIM por el correo fundeimucv@gmail.com</p>"
-		normative += "<p>2.  La participación de los estudiantes en las actividades del foro es obligatoria y será tomada como asistencia a clase. La ausencia en el foro por 2 semanas, no necesariamente de manera consecutiva, tendrá como consecuencia la pérdida del curso por inasistencia.</p>"
-		normative += "<p>3.  La calificación mínima aprobatoria es de 15 puntos. La evaluación será continua y dinámica. Encontrará mayor información en el cronograma del curso incluido en el módulo introductorio del nivel.</p>"
-		normative += "<p>4.  El programa ONLINE consta de solo actividades asíncronas, es decir, no serán clases en vivo,  para que cada estudiante pueda organizarse y buscar el tiempo y la conexión para seguir formándose a su ritmo y en el horario de su preferencia; sin embargo, cada estudiante debe completar dos clases por semana, además de realizar las tareas asignadas y los exámenes semanales programados.</p>"
-
-		$('#confirmDialog #content').html(textConfirm + normative);
-
-		$('#confirmDialog #confirmLink').attr('href', url);
-		$('#confirmDialog').modal();
-
-	})
 
 
 });
