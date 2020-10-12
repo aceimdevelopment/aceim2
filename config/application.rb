4616a9a5fc5c1c1022d996f565694bcbb0f52faa
Daniel Moros <danielousky@gmail.com>
2020-10-12 07:59:23 -0400

    ajuste

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module AceimReact
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :es
     config.time_zone = "Caracas"
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
