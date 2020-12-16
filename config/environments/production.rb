a66c52c3acad55ea33121f2d82539b82d05e76d4
Daniel Moros <danielousky@gmail.com>
2020-12-15 22:00:39 -0400

    Ajuste default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # URL Options:
-  config.action_mailer.default_url_options = { host: 'fundeim.com' }
+  config.action_mailer.default_url_options = { host: 'aceim.fundeim.com' }
 
   config.active_storage.service = :digitalocean #:local
   # Mailgun Config:
